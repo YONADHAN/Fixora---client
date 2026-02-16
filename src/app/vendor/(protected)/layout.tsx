31492c111937d3120e94bb4c96623679a06ac629
Yonadhan <yonadhanmm77@gmail.com>
2026-02-16 13:17:07 +0530

    fix(vendor verification): remove commented out old codes

--- src/app/vendor/(protected)/layout.tsx
+++ src/app/vendor/(protected)/layout.tsx
@@ -1,14 +1,4 @@
-// 'use client'
 
-// import AuthGuard from '@/components/auth/AuthGuard'
-
-// export default function VendorProtectedLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return <AuthGuard role='vendor'>{children}</AuthGuard>
-// }
 
 'use client'
 
