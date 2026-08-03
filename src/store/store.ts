e1e6702200fc08e97a294b35395e12ae7da58875
Yonadhan <yonadhanmm77@gmail.com>
2026-08-03 10:52:35 +0530

    fix: Added noop storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,25 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { persistStore } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage'
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key: string) {
+      return Promise.resolve(null)
+    },
+    setItem(_key: string, value: any) {
+      return Promise.resolve(value)
+    },
+    removeItem(_key: string) {
+      return Promise.resolve()
+    },
+  }
+}
+
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('local')
+    : createNoopStorage()
 import persistReducer from 'redux-persist/es/persistReducer'
 import { useDispatch } from 'react-redux'
 import customerReducer from './slices/customer.slice'
